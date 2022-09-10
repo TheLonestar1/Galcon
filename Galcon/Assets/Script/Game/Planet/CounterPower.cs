0df5cd7f3b372b2d12f9156a1cae3a3107ea54a6
TheLonestar1 <yura2000077@yandex.ru>
2022-09-10 20:02:03 +0700

    Add all Sprite and logic for counter ships on planets

---
+++ Galcon/Assets/Script/Game/Planet/CounterPower.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class CounterPower : MonoBehaviour
+{
+    [SerializeField]
+    int _shipCounter;
+    [SerializeField,Range(0,10)]
+    float _timer;
+    [SerializeField]
+    int _addShips;
+    // Start is called before the first frame update
+    public int ships { get { return _shipCounter; } set { _shipCounter = value; } }
+    TMP_Text _fieldText;
+    void Start()
+    {
+        _fieldText = GetComponentInChildren<TMP_Text>();
+        _fieldText.text = _shipCounter.ToString();
+        if (this.gameObject.tag != "neutral")
+            StartCoroutine(Counter());
+    }
+
+    IEnumerator Counter()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_timer);
+            _shipCounter += _addShips;
+            _fieldText.text = _shipCounter.ToString();
+        }
+    }
+}
